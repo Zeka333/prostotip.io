33a0bfcf0ce261ce59b0afca79216b7f8b2ce1c8
Zeka333 <105851599+Zeka333@users.noreply.github.com>
2025-09-18 22:50:38 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,15 +3,15 @@ var isRightsEnabled = false; // Права
 var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "06.08.1488"; // Дата рождения
-var fullname = "Підар Єдік Пєрдановіч" // ФИО
-var name = "Єдік" // Имя
+var birthdate = "06.04.2007"; // Дата рождения
+var fullname = "Гавриленко Артем " // ФИО
+var name = "Артем" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "304510087" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "9107159306" // ИНН
 
 // Права
 var rights_valid_until = "01.01.2022" // Права "Дійсні до"
@@ -23,3 +23,4 @@ var rights_id = "KBE968639" // Права "Серія та номер""
 var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
 
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
+
